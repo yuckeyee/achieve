f8ea2ec4473b739bec63019d2fc3922d3977c7db
Yuki Kayama <kayama0204@gmail.com>
2017-07-04 02:48:13 +0000

    finished cep09

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |n|
+  name  = "name#{n}"
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(
+               name: name,
+               email: email,
+               password: password,
+               password_confirmation: password
+               )
+end
+
+n = 1
+while n <= 100
+  Blog.create(
+    title: "title#{n}",
+    content: "content#{n}",
+    user_id: n
+  )
+  n = n + 1
+end
